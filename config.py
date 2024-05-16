3709845f44876bd92eb482423a83b5fd92d42e2e
Silviu Stroe <silviu@silviu-s.com>
2024-05-16 20:49:09 +0300

    copyright and small refactorings

--- config.py
+++ config.py
@@ -1,10 +1,28 @@
+#  Copyright (c) 2024 by Silviu Stroe (brainic.io)
+#  #
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  #
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#  #
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#  #
+#  Created on 5/16/24, 8:44 PM
+#  #
+#  Author: Silviu Stroe
+
 import os
 import json
 
 
 def load_settings():
-    initial_data = {'buttons': [{'label': 'Button 1', 'action': '#', 'color': '#4CAF50'},
-                                {'label': 'Button 2', 'action': '#', 'color': '#2196F3'}],
+    initial_data = {'buttons': [],
                     'columns': 2,
                     'app_background': '#f0f0f0'}
 
