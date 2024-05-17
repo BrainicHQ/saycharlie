abc139e8f3032dbb9425ac44dba43b442c68beef
Silviu Stroe <silviu@silviu-s.com>
2024-05-17 11:17:06 +0300

    improve install.sh, requirements.txt update

--- install.sh
+++ install.sh
@@ -26,9 +26,21 @@ APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Starting the installation of the saycharlie SVX Dashboard..."
 
-# Check if Python 3 and pip are installed
-command -v python3 &>/dev/null || { echo "Python 3 is not installed. Please install Python 3."; exit 1; }
-command -v pip3 &>/dev/null || { echo "pip3 is not installed. Please install pip3."; sudo apt-get update && sudo apt-get install python3-pip -y; }
+# Install Python 3, pip, and python3-venv if they are not already installed
+if ! command -v python3 &>/dev/null; then
+    echo "Python 3 is not installed. Installing Python 3..."
+    sudo apt-get update && sudo apt-get install python3 -y
+fi
+
+if ! command -v pip3 &>/dev/null; then
+    echo "pip3 is not installed. Installing pip3..."
+    sudo apt-get install python3-pip -y
+fi
+
+if ! dpkg -s python3-venv &>/dev/null; then
+    echo "python3-venv is not installed. Installing python3-venv..."
+    sudo apt-get install python3-venv -y
+fi
 
 # Update pip to the latest version
 echo "Updating pip to the latest version..."
@@ -44,7 +56,7 @@ source "$APP_DIR/venv/bin/activate"
 
 # Install requirements
 echo "Installing dependencies from requirements.txt..."
-pip3 install -r "$APP_DIR/requirements.txt"
+pip install -r "$APP_DIR/requirements.txt"
 echo "Dependencies installed."
 
 # Creating a systemd service file
@@ -79,7 +91,10 @@ sudo systemctl start saycharlie.service
 
 echo "saycharlie service has been started and enabled at boot."
 
+LOCAL_IP=$(hostname -I | awk '{print $1}') # Get the local IP address
+
 # Provide final confirmation and instructions
 echo "Installation and service setup complete. saycharlie is now running as a service."
 echo "You can check the status of the service with 'sudo systemctl status saycharlie.service'"
-echo "Access saycharlie Dashboard at: http://saycharlie.local:8337 or http://localhost:8337"
+echo "Access saycharlie Dashboard at: http://saycharlie.local:8337 or http://localhost:8337 or http://$LOCAL_IP:8337"
+echo "To stop the service, run 'sudo systemctl stop saycharlie.service'"
